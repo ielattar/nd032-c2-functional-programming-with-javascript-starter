7f4e688bc165a409a3e3e4d7e61e1f0837458449
Ihab El Attar <Ihab.ElAttar@york.ca>
2022-08-22 21:15:33 -0400

    deleted apod section
    deleted apod section

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -27,19 +27,10 @@ const App = (state) => {
     return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
+            
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+              
+               
 				${RoversDetails(rovers,active_rover,data,images)}
             </section>
         </main>
@@ -128,8 +119,9 @@ const RoversDetails = (rovers, active_rover,data,images)=>{
 	
 	
 	images_html=`<br/>
+			<p> <b> Latest Photo Date : </b> ${images && images.rover.latest_photos[0].earth_date}</p>
 			<img src="${images && images.rover.latest_photos[0].img_src}" height="100%" width="100%" />
-			<p>${images && images.rover.latest_photos[0].earth_date}</p>
+			
 	
 	`;
 	
